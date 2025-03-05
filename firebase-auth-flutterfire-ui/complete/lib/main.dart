f043ec10fcd77b397b1033bd27d6da5011ad520a
Sandro Martínez <virgosmp.mx@gmail.com>
2025-03-04 21:32:23 -0600

    oops

--- firebase-auth-flutterfire-ui/complete/lib/main.dart
+++ firebase-auth-flutterfire-ui/complete/lib/main.dart
@@ -5,7 +5,7 @@ import 'app.dart';
 import 'firebase_options.dart';
 
 // TODO(codelab user): Get API key
-const clientId = 'YOUR_CLIENT_ID';
+const clientId = 'AIzaSyCAgP9E4B14DB-0uCv31uRfl5hph9_jx_U';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
